96e9f6f64831c7ba13da858cfbf7bdafdbb83092
Roland Bergler <roland@bergler.net>
2017-12-06 07:58:34 +0100

    working unit tests for base functionality

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,15 +1,19 @@
-import $ from 'jquery';
-import './puzzle.css';
-
-var puzzle = function () {
-    let TILESIZE;
+var Puzzle = function () {
+    "use strict";
     let NCOLUMNS = 4;
     let NFIELDS = NCOLUMNS * NCOLUMNS;
 
-    let field = randomizeField();
-    //[null,5,2,15,8,1,4,14,7,6,3,13,12,11,10,9];
+    function Puzzle() {
 
-    function randomizeField() {
+        this.field = [];
+        //[null,5,2,15,8,1,4,14,7,6,3,13,12,11,10,9];
+    }
+
+    function _getField() {
+        return this.field;
+    }
+
+    function _randomizeField() {
         while (true) {
             let f = [null];
             let indexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
@@ -18,13 +22,37 @@ var puzzle = function () {
                 f.push(indexes[i]);
                 indexes.splice(i, 1);
             }
-            if (isSolvable(f))
+            if (_isSolvable(f))
                 return f;
         }
     }
 
+    function _init(f) {
+        if (f === undefined) {
+            f = _randomizeField();
+        } else {
+            _checkField(f);
+        }
+
+        this.field = f;
+    }
+
+    function _checkField(f) {
+        if (f.length !== NFIELDS)
+            throw ("field has wrong length");
+        for (let i = 1; i < NFIELDS; i++) {
+            if (!f.includes(i))
+                throw ("array is incomplete, value " + i + " is missing");
+        }
+        if (!f.includes(null))
+            throw ("array must have null field");
+
+        if (!_isSolvable(f))
+            throw ("field is not a solvable puzzle");
+    }
+
     // Algorithmus nach https://de.wikipedia.org/wiki/15-Puzzle
-    function isSolvable(f) {
+    function _isSolvable(f) {
         let nullIndex = f.findIndex(function (e) {
             return e === null;
         });
@@ -44,35 +72,35 @@ var puzzle = function () {
         return (n1 + n2) % 2 == 1;
     }
 
-    function findTile(tileIndex) {
+    function _findTile(tileIndex) {
         for (let i = 0; i < NFIELDS; i++) {
-            if (field[i] === tileIndex) {
+            if (this.field[i] === tileIndex) {
                 return i;
             }
         }
         return null;
     }
 
-    function freeSpaceNextTo(pos) {
-        if (field[pos - 1] === null) {
+    function _freeSpaceNextTo(pos) {
+        if (this.field[pos - 1] === null && pos % NCOLUMNS > 0) {
             return {
                 index: pos - 1,
                 direction: "left"
             };
         }
-        if (field[pos + 1] === null) {
+        if (this.field[pos + 1] === null && pos % NCOLUMNS < NCOLUMNS - 1) {
             return {
                 index: pos + 1,
                 direction: "right"
             };
         }
-        if (field[pos - NCOLUMNS] === null) {
+        if (this.field[pos - NCOLUMNS] === null) {
             return {
                 index: pos - NCOLUMNS,
                 direction: "up"
             };
         }
-        if (field[pos + NCOLUMNS] === null) {
+        if (this.field[pos + NCOLUMNS] === null) {
             return {
                 index: pos + NCOLUMNS,
                 direction: "down"
@@ -81,76 +109,37 @@ var puzzle = function () {
         return null;
     }
 
-    function onTileClick(tile, tileIndex) {
-        changeImage();
-        let pos = findTile(tileIndex);
+    function _moveTile(tileIndex) {
+        let pos = _findTile.call(this, tileIndex);
         if (pos !== null) {
-            let newpos = freeSpaceNextTo(pos);
+            let newpos = _freeSpaceNextTo.call(this, pos);
             if (newpos !== null) {
-                showTileOnPos(tile, newpos.index);
-                tile.removeClass("moveleft moveright moveup movedown");
-                tile.addClass("move" + newpos.direction);
-                field[pos] = null;
-                field[newpos.index] = tileIndex;
+                this.showTileOnPos(tileIndex, newpos.index, newpos.direction);
+                this.field[pos] = null;
+                this.field[newpos.index] = tileIndex;
             }
         }
-        if (isSolved()) {
-            field[0] = 0;
-            $("#tile0").show();
-        }
     }
 
-    function showTileOnPos(tile, index) {
-        let x = index % NCOLUMNS;
-        let y = Math.floor(index / NCOLUMNS);
-        tile.css("left", "" + (x * TILESIZE) + "px");
-        tile.css("top", "" + (y * TILESIZE) + "px");
-    }
-
-    function paintTiles() {
-        for (let tileIdx = 0; tileIdx < NFIELDS; tileIdx++) {
-            let tile = $("#tile" + tileIdx);
-            let x = tileIdx % NCOLUMNS;
-            let y = Math.floor(tileIdx / NCOLUMNS);
-            tile.css("background-position", "-" + (TILESIZE * x) + "px -" + (TILESIZE * y) + "px");
-        }
-    }
-
-    function changeImage() {
-        for (let tileIdx = 0; tileIdx < NFIELDS; tileIdx++) {
-            let tile = $("#tile" + tileIdx);
-            tile.toggleClass("alternateImage");
-        }
-    }
-
-    function isSolved() {
+    function _isSolved() {
         for (let index = 1; index < NFIELDS; index++) {
-            if (field[index] !== index) {
+            if (this.field[index] !== index) {
                 return false;
             }
         }
         return true;
     }
 
-    return {
-        init: function () {
-            TILESIZE = $("#tile0").width();
-            paintTiles();
-            $("#tile0").hide();
-            for (let index = 1; index < NFIELDS; index++) {
-                let tileIdx = field[index];
-                if (tileIdx != null) {
-                    let tile = $("#tile" + tileIdx)
-                    showTileOnPos(tile, index);
-                    tile.click(function () {
-                        onTileClick(tile, tileIdx);
-                    });
-                }
-            }
-        }
-    };
-}();
+    function _showTileOnPos(tileIndex, posIndex, direction) {
 
-$(document).ready(function () {
-    puzzle.init();
-});
+    }
+
+
+    Puzzle.prototype.isSolved = _isSolved;
+    Puzzle.prototype.init = _init
+    Puzzle.prototype.getField = _getField
+    Puzzle.prototype.moveTile = _moveTile
+    Puzzle.prototype.showTileOnPos = _showTileOnPos
+
+    return Puzzle;
+}();
---
+++ test/puzzle.test.js
@@ -0,0 +1,136 @@
+describe('Puzzle', function () {
+
+    describe('#init()', function () {
+
+        let puzzle;
+
+        beforeEach(function () {
+            puzzle = new Puzzle();
+        })
+
+
+        it('result should have free field on top left', function () {
+            puzzle.init()
+            let field = puzzle.getField()
+            assert.equal(field[0], null)
+        });
+
+        it(' should provide different value on each call', function () {
+            puzzle.init()
+            let field = puzzle.getField()
+            puzzle.init()
+            let field2 = puzzle.getField()
+            assert.notDeepEqual(field, field2)
+        });
+    });
+
+
+    describe('#init(array)', function () {
+
+        let puzzle;
+
+        beforeEach(function () {
+            puzzle = new Puzzle();
+        })
+
+        it('call works', function () {
+            puzzle.init([null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        });
+
+
+        it('argument must have empty field', function () {
+            assert.throws(function () {
+                puzzle.init([1, 16, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            });
+        });
+
+        it('argument must contain all values', function () {
+            assert.throws(function () {
+                puzzle.init([null, 1, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            });
+        });
+
+        it('argument must have correct number of fields', function () {
+            assert.throws(function () {
+                puzzle.init([null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+            });
+        });
+
+        it('argument must describe solvable puzzle', function () {
+            assert.throws(function () {
+                puzzle.init([null, 2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            });
+        });
+    });
+
+    describe('#moveTile()', function () {
+
+        let puzzle;
+
+        beforeEach(function () {
+            puzzle = new Puzzle();
+        });
+
+        it('moves tile to the left', function () {
+            puzzle.init([null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(1);
+            assert.deepEqual(puzzle.getField(), [1, null, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+
+        it('moves tile to the right', function () {
+            puzzle.init([1, null, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(1);
+            assert.deepEqual(puzzle.getField(), [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+
+        it('moves tile down', function () {
+            puzzle.init([1, 2, 4, 3, null, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(1);
+            assert.deepEqual(puzzle.getField(), [null, 2, 4, 3, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+
+        it('moves tile up', function () {
+            puzzle.init([null, 2, 4, 3, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(1);
+            assert.deepEqual(puzzle.getField(), [1, 2, 4, 3, null, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+
+        it('doesnt moves tile without free neighbour', function () {
+            puzzle.init([null, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(1);
+            assert.deepEqual(puzzle.getField(), [null, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+
+        it('doesnt wrap move right', function () {
+            puzzle.init([1, 2, 4, 3, null, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(3);
+            assert.deepEqual(puzzle.getField(), [1, 2, 4, 3, null, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+        it('doesnt wrap move left', function () {
+            puzzle.init([1, 2, 3, null, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            puzzle.moveTile(4);
+            assert.deepEqual(puzzle.getField(), [1, 2, 3, null, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+        })
+    });
+
+
+    describe('#isSolved()', function () {
+        let puzzle;
+
+        beforeEach(function () {
+            puzzle = new Puzzle();
+        });
+
+        it('returns false', function () {
+            puzzle.init([1, null, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            let solved = puzzle.isSolved();
+            assert.isFalse(solved)
+        })
+
+        it('returns true', function () {
+            puzzle.init([null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            let solved = puzzle.isSolved();
+            assert.isTrue(solved)
+        })
+    });
+});
